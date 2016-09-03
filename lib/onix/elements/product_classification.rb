00e58a608aa7b006de073e81568b05d973f0142e
tim <stuff@milkfarmproductions.com>
2016-09-02 20:06:27 -0700

    Employ onix_decimal_accessor instead of xml_accessor with Float

--- lib/onix/elements/product_classification.rb
+++ lib/onix/elements/product_classification.rb
@@ -4,5 +4,5 @@ class ONIX::ProductClassification < ONIX::Element
   xml_name "ProductClassification"
   onix_code_from_list :product_classification_type, "ProductClassificationType", :list => 9
   xml_accessor :product_classification_code, :from => "ProductClassificationCode"
-  xml_accessor :percent, :from => "Percent", :as => Float
+  onix_decimal_accessor :percent, 'Percent'
 end
